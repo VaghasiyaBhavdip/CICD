757c8b62d92fc307e5b51b8315a807f31a9144e2
bhavdip <bhavdip.v@optimumfintech.co.in>
2024-03-08 11:44:09 +0530

    Desploy

--- generate-aab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Path to the generated AAB file
-AAB_FILE="./android/app/build/outputs/bundle/release/app-release.aab"
-
-# Command to build the AAB using bundletool
-bundletool='bundletool-all-1.10.0.jar' # Adjust this to your bundletool version
-java -jar $bundletool build-apks --bundle=./android/app/build/outputs/bundle/release/app-release.aab --output=$AAB_FILE --overwrite
